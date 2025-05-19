15b3e2e5beb61b5503b30cd4f9e2f3392e0c76a5
BluiWulf <bluiwulf@gmail.com>
2025-05-19 01:29:04 -0400

    adding Point and Line classes

--- graphics.py
+++ graphics.py
@@ -22,3 +22,24 @@ class Window:
 
     def close(self):
         self.__running = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+
+class Point:
+    def __init__(self, x, y):
+        # x = 0 is the left of the screen
+        # y = 0 is the top of the screen
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, pt_A, pt_B):
+        self.pt_A = pt_A
+        self.pt_B = pt_B
+
+    def draw(self, Canvas, fill_color):
+        self.canvas.create_line(
+            self.pt_A.x, self.pt_A.y, self.pt_B.x, self.pt_B.y, fill = self.fill_color, width = 2
+        )
+
